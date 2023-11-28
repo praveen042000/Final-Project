b88b146a49a582ab10e013485e30085b5c6107b4
praveen042000 <146086471+praveen042000@users.noreply.github.com>
2023-11-28 17:22:52 +0530

    28/11/2023

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -22,7 +22,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
  
-@CucumberOptions(features="src/test/resources/Feature",plugin = {"json:target/cucumber-report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+@CucumberOptions(features="src/test/resources/Feature",plugin = {"json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 glue="stepDefinition")
 public class CucumberRunner extends AbstractTestNGCucumberTests {
  
